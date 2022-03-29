02b3e2f031940e3de7d676c1970cc2ef0c6da22d
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2022-03-29 20:36:19 +0700

    SDA6

--- kelompok 6 sda/src/binaryTree.java
+++ kelompok 6 sda/src/binaryTree.java
@@ -2,4 +2,21 @@
 
 public class binaryTree {
 
+    public String root;
+
+    public void addNode(Node node) {
+    }
+
+    public static String searchValue(binaryTree pencarian, String nama1) {
+        return null;
+    }
+
+    public static String searchValue(String root2, String nama1) {
+        return null;
+    }
+
+    public static String searchValue(String root2, int nama1) {
+        return null;
+    }
+
 }
